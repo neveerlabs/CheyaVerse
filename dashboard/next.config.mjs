2dc1bb8637509e2ee745782afec491c4f20722a7
M. Syalman Al Farizi <217960416+neveerlabs@users.noreply.github.com>
2026-09-20 21:58:42 +0700

    Update next.config.mjs

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  output: "standalone",
   experimental: { optimizePackageImports: ["lucide-react"] },
 };
 export default nextConfig;
