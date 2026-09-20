0e71174468996982064c75232dd0af0ce4b257d8
M. Syalman Al Farizi <217960416+neveerlabs@users.noreply.github.com>
2026-09-21 04:31:55 +0700

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  experimental: { optimizePackageImports: ["lucide-react"] },
-};
-export default nextConfig;
