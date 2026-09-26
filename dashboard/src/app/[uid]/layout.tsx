13ff234671657fb5a022457833311d32b531d155
M. Syalman Al Farizi <217960416+neveerlabs@users.noreply.github.com>
2026-09-26 15:16:18 +0700

    Update layout

--- dashboard/src/app/[uid]/layout.tsx
+++ dashboard/src/app/[uid]/layout.tsx
@@ -2,6 +2,7 @@ import { TabBar } from "@/components/TabBar";
 import { RealtimeSync } from "@/components/RealtimeSync";
 import { SessionInit } from "@/components/SessionInit";
 import { DeviceNotifications } from "@/components/DeviceNotifications";
+import { PushRegister } from "@/components/PushRegister";
 
 export const dynamic = "force-dynamic";
 
@@ -19,6 +20,7 @@ export default async function UserLayout({
     <>
       {valid && <SessionInit uid={params.uid} />}
       {valid && <DeviceNotifications uid={params.uid} />}
+      {valid && <PushRegister uid={params.uid} />}
       <RealtimeSync uid={params.uid} />
       <main className="mx-auto max-w-[600px] px-5 pb-[calc(74px+env(safe-area-inset-bottom))]">
         {children}
